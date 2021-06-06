e3423ee21d8353f8dfbf14b730fcd50b8923c4d3
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-06 17:22:36 +0530

    Add:Welcome to the Generic Test

---
+++ src/main/java/com/generictest/MaxValue.java
@@ -0,0 +1,7 @@
+package com.generictest;
+
+public class MaxValue {
+    public static void main(String[] args) {
+        System.out.println("Welcome To the Generic Test :");
+    }
+}
---
+++ src/test/java/com/generictest/TestClass.java
@@ -0,0 +1,5 @@
+package com.generictest;
+
+public class TestClass {
+
+}
