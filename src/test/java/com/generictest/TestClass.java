1f48bd6ac885f24f8c67f33b36491a8157968756
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-06 17:52:09 +0530

    Add:1.1,1.2,1.3 Using Test Cases

--- src/test/java/com/generictest/TestClass.java
+++ src/test/java/com/generictest/TestClass.java
@@ -1,5 +1,28 @@
 package com.generictest;
 
-public class TestClass {
+import org.junit.Assert;
+import org.junit.Test;
 
+
+public class TestClass {
+    MaxValue findMax = new MaxValue();
+
+    @Test
+    public void givenFirstNumberAsMaxNumber_ShouldReturn_FirstNumberAsMax() {
+
+        Integer maximum = findMax.testMaximumInteger(75, 545, 575);
+        Assert.assertEquals((Integer) 575, maximum);
+    }
+
+    @Test
+    public void givenSecondNumberAsMaxNumber_ShouldReturn_SecondNumberAsMax() {
+        Integer maximum1 = findMax.testMaximumInteger(78, 34, 21);
+        Assert.assertEquals((Integer) 78, maximum1);
+    }
+
+    @Test
+    public void givenThirdNumberAsMaxNumber_ShouldReturn_ThirdNumberAsMax() {
+        Integer maximum2 = findMax.testMaximumInteger(856, 4453, 34);
+        Assert.assertEquals((Integer) 4453, maximum2);
+    }
 }
