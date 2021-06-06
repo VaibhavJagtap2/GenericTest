8cabc59831fd7a27c56a4efd6491c5eb9901fd43
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-06 17:32:45 +0530

    Add:Find maximum number

--- src/main/java/com/generictest/MaxValue.java
+++ src/main/java/com/generictest/MaxValue.java
@@ -2,6 +2,19 @@ package com.generictest;
 
 public class MaxValue {
     public static void main(String[] args) {
-        System.out.println("Welcome To the Generic Test :");
+        printMax(testMaximumInteger(200, 300, 500));
+    }
+    public static int testMaximumInteger(Integer x, Integer y, Integer z){
+        Integer max = x;
+        if (y.compareTo(max) > 0){
+            max = y;
+        }
+        if (z.compareTo(max) > 0){
+            max = z;
+        }
+        return max;
+    }
+    private static void printMax(Integer max){
+        System.out.println("Maximum number is : " + max);
     }
 }
