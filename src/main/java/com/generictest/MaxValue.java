2273179c7e51d5bf747620b6c5baa36eadaee6e9
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-07 14:16:10 +0530

    Add:Generic method to find maximum

--- src/main/java/com/generictest/MaxValue.java
+++ src/main/java/com/generictest/MaxValue.java
@@ -6,6 +6,14 @@ public class MaxValue {
         Double [] a = {1.1,1.2,1.3};
         String [] b = {"Vaibhav", "Suraj", "Parag"};
     }
+    public static <E extends Comparable<E>> E max(E[] list){
+        E max = list[0];
+        for (int i=1; i<list.length; i++){
+            if (list[i].compareTo(max) > 0)
+                max = list[i];
+        }
+        return max;
+    }
     public static int testMaximumInteger(Integer x, Integer y, Integer z){
         Integer max = x;
         if (y.compareTo(max) > 0){
