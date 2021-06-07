1cdef673acb7e26e79a924ac706a38e7e1ed11cb
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-07 12:45:01 +0530

    Add:Check Maximum Length Of String

--- src/main/java/com/generictest/MaxValue.java
+++ src/main/java/com/generictest/MaxValue.java
@@ -3,6 +3,8 @@ package com.generictest;
 public class MaxValue {
     public static void main(String[] args) {
         printMax(testMaximumInteger(200, 300, 500));
+        Double [] a = {1.1,1.2,1.3};
+        String [] b = {"Vaibhav", "Suraj", "Parag"};
     }
     public static int testMaximumInteger(Integer x, Integer y, Integer z){
         Integer max = x;
@@ -14,7 +16,37 @@ public class MaxValue {
         }
         return max;
     }
+    public static double testMaximumDouble(Double x, Double y, Double z){
+        Double max1 = x;
+        if (y.compareTo(max1) > 0){
+            max1 = y;
+        }
+        if (z.compareTo(max1) > 0){
+            max1 = z;
+        }
+        return max1;
+    }
+    public String testMaximumString(String first, String second, String third) {
+        int max2 = first.length();
+        String letter = first;
+        if (second.length() > max2) {
+            max2 = second.length();
+            letter = second;
+        }
+        if (third.length() > max2) {
+            max2 = third.length();
+            letter = third;
+        }
+        printMax(letter);
+        return letter;
+    }
     private static void printMax(Integer max){
         System.out.println("Maximum number is : " + max);
+    }
+    private static void printMax(Double max){
+        System.out.println("Maximum number is : " + max);
+    }
+    private static void printMax(String max) {
+        System.out.println(" Maximum number is : " + max);
     }
 }
--- src/test/java/com/generictest/TestClass.java
+++ src/test/java/com/generictest/TestClass.java
@@ -9,7 +9,6 @@ public class TestClass {
 
     @Test
     public void givenFirstNumberAsMaxNumber_ShouldReturn_FirstNumberAsMax() {
-
         Integer maximum = findMax.testMaximumInteger(75, 545, 575);
         Assert.assertEquals((Integer) 575, maximum);
     }
@@ -24,5 +23,38 @@ public class TestClass {
     public void givenThirdNumberAsMaxNumber_ShouldReturn_ThirdNumberAsMax() {
         Integer maximum2 = findMax.testMaximumInteger(856, 4453, 34);
         Assert.assertEquals((Integer) 4453, maximum2);
+    }
+
+    @Test
+    public void givenFirstFloatAsMaxNumber_ShouldReturnFirstFloatAsMaximum() {
+        Double max1 = findMax.testMaximumDouble(20.00, 25.00, 57.00);
+        Assert.assertEquals((Double) 57.00, max1);
+    }
+
+    @Test
+    public void givenSecondFloatAsMaxNumber_ShouldReturnSecondFloatAsMaximum() {
+        Double max1 = findMax.testMaximumDouble(7.00, 12.00, 15.44);
+        Assert.assertEquals((Double) 15.44, max1);
+    }
+
+    @Test
+    public void givenThirdFloatAsMaxNumber_ShouldReturnThirdFloatAsMaximum() {
+        Double max1 = findMax.testMaximumDouble(71.00, 129.00, 125.44);
+        Assert.assertEquals((Double) 129.00, max1);
+    }
+    @Test
+    public void givenFirstLetterAsMaximum_ShouldReturnFirstLetterAsMaximum(){
+        String max2 = findMax.testMaximumString("Vaibhav", "Suraj", "Parag");
+        Assert.assertEquals((String) "Vaibhav", max2);
+    }
+    @Test
+    public void givenSecondLetterAsMaximum_ShouldReturnSecondLetterAsMaximum(){
+        String max2 = findMax.testMaximumString("Suraj", "Vaibhav", "Parag");
+        Assert.assertEquals((String) "Vaibhav", max2);
+    }
+    @Test
+    public void givenThirdLetterAsMaximum_ShouldReturnThirdLetterAsMaximum(){
+        String max2 = findMax.testMaximumString("Vaibhav", "Parag", "Suraj");
+        Assert.assertEquals((String) "Vaibhav", max2);
     }
 }
